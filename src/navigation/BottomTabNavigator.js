284776874ac6662d7509cb0b21b809ca5f1f3b06
zannash <zanaashehu@gmail.com>
2024-03-27 15:04:50 +0100

    Module 23 and 24 IOS and Android Screens

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {MainStackNavigator,AboutStackNavigator} from './StackNavigator';
+import {MainStackNavigator,AboutStackNavigator,IosStackNavigator} from './StackNavigator';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Home from '../screens/Home';
-import About from '../screens/About';``
+import About from '../screens/About';
+import Android  from '../screens/Android';
+import Ios from '../screens/Ios';
 
 
 const Tab = createBottomTabNavigator();
@@ -35,13 +37,25 @@ tabBarOptions = {
     }
     />
 
-    <Tab.Screen
-     name = "About view"
-     component = {About}
+ 
+            <Tab.Screen 
+                name="Ios"
+                component={Ios}
+                options={{
+                    tabBarLabel: 'iOS',
+                    tabBarIcon: ({color}) => (
+                        <Icon name="apple" size={26} color={color} />
+                    )
+                }}
+            />
+
+<Tab.Screen
+     name = "Android"
+     component = {Android}
      options = {
          {
-             tabBarLabel: 'About',
-             tabBarIcon:({color}) => (<Icon name="rocket" size={26} color={color}/>),
+             tabBarLabel: 'Android',
+             tabBarIcon:({color}) => (<Icon name="android" size={26} color={color}/>),
          }
      }
 
---
+++ src/screens/Ios.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList, ScrollView, TouchableOpacity } from 'react-native';
+import data from '../data/data.json';
+import Item from '../components/Item';
+
+class Ios extends React.Component{ 
+
+    constructor(){
+        super();
+        this.state = {
+            product: []
+        }
+    }
+
+    componentDidMount(){
+        this.setState({
+            product: data
+        });
+    }
+
+    render(){
+        return(
+            <ScrollView>
+                <View style={styles.container}>
+                    <Text style={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Text>
+                    <View style={styles.productsContainer}>
+                        <FlatList 
+                            data={this.state.product.ios}
+                            renderItem = {({item}) => (
+                                <Item item={item}/>
+                            )}
+                        />
+                        <TouchableOpacity style={styles.btn}>
+                            <Text style={styles.btnText}>View More</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ScrollView>
+           
+        );
+    }
+       
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        alignSelf: 'center',
+        padding: 20
+    },
+    desc: {
+        marginBottom: 20
+    },
+});
+
+export default Ios;
